4d0fa9302ab7df4ec9adf3a15c4412de0d3a07b2
lukas <63365889+lukgth@users.noreply.github.com>
2021-06-02 17:02:17 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -17,7 +17,6 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'm787057886-564a4e8804e949cbd153dfc7',
-    'm787498548-1eea93a149c45ce2e4c4d34f',
     'm787701639-4ec42fedc2d43e9d790cd54f',
     'm787057889-2b803251c9316e7843b8564a',
   ],
